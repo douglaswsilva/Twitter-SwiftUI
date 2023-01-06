ec09d05b4048e68e44bb987eada114ceea9e926e
Douglas Silva <silvadouglasw@gmail.com>
2023-01-06 13:15:46 -0300

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Douglas Silva on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
